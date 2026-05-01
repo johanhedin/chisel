9c45ede24e8ba0d689a6a56c5ac0662cb2c3170a
Johan Hedin <johan.o.hedin@gmail.com>
2026-05-01 21:15:19 +0200

    Extend lazy filter with optional_readings

--- test/registration_lazy_filter.hpp
+++ test/registration_lazy_filter.hpp
@@ -7,10 +7,27 @@ inline void chisel_test_filter(Registration::Reader& r, bool& match) {
         auto st = item.read_sensor_type();
         if (!st.empty() && (st[0] == 'A' || st[0] == 'a')) match = true;
 
-        item.skip_remaining();
+        // skip_remaining() is not needed since it is handled under the hood
+        //item.skip_remaining();
 
         return !match;
     });
+
+    r.skip_extra_readings();
+
+    auto optional_readings = r.read_optional_readings();
+    if (optional_readings.has_value()) {
+        optional_readings.for_each([&](Registration::Item::Reader& item) {
+            item.skip_timestamp();
+            auto st = item.read_sensor_type();
+            if (!st.empty() && (st[0] == 'D' || st[0] == 'd')) match = true;
+
+            // skip_remaining() is not needed since it is handled under the hood
+            //item.skip_remaining();
+
+            return !match;
+        });
+    }
 
     r.skip_remaining();
 }
